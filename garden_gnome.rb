d22e054999df7a12e300e2065e09469930b3301d
tammy0508 <tamara.dandreamatteo@gmail.com>
2018-07-13 18:23:06 +0000

    automatically backed up by learn

--- garden_gnome.rb
+++ garden_gnome.rb
@@ -3,10 +3,10 @@ class GardenGnome
   attr_accessor :name , :age , :gluten_allergy , :personality
 
   def initialize
-    @personality=personality
+    @personality="evil"
   end
 end
 
-gnome = GardenGnome.new
-gnome.personality="evil"
+# gnome = GardenGnome.new
+# gnome.personality="nice"
 # puts gnome.name
