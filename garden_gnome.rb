a84b6bf1c51a8ba09d6b06567b9a2c5d620029be
tammy0508 <tamara.dandreamatteo@gmail.com>
2018-07-13 18:35:08 +0000

    automatically backed up by learn

--- garden_gnome.rb
+++ garden_gnome.rb
@@ -1,12 +1,19 @@
 class GardenGnome
 
-  attr_reader :name , :age , :gluten_allergy , :personality ,:hat_color
+  attr_reader :name , :age , :gluten_allergy , :personality ,:hat_color ,
   attr_writer :name , :age , :gluten_allergy
 
   def initialize(hat_color = "red")
     @personality="evil"
     @hat_color= hat_color
+    @gnaws = gnaws 
   end
+
+  def ganws 
+    puts "Gnawing on a tree!!!"
+  end 
+
+
   #
   # def hat_color
   #   @hat_color="red"
