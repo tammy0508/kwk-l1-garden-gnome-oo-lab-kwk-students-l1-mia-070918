554fba1cf13ea49b80529327d7cbab7ebde93164
tammy0508 <tamara.dandreamatteo@gmail.com>
2018-07-13 18:11:38 +0000

    automatically backed up by learn

--- garden_gnome.rb
+++ garden_gnome.rb
@@ -2,13 +2,12 @@ class GardenGnome
 
   attr_accessor :name , :age , :gluten_allergy
 
-  # def initialize
-  #   @name= name
-  # end
+  def initialize(name)
+    @name= name
+  end
 
 
 end
 
-gnome = GardenGnome.new
-gnome.name="joe"
+gnome = GardenGnome.new("Joe")
 puts gnome.name
